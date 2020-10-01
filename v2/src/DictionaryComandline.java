021e43fad67e6d7fc8d26ab7f6210ad5bc91a771
vubom01 <lehuyvu18052001>
2020-10-01 18:02:40 +0700

    Cai tien lan 1

---
+++ v2/src/DictionaryComandline.java
@@ -0,0 +1,26 @@
+public class DictionaryComandline {
+
+    DictionaryManagement DicM = new DictionaryManagement();
+
+    DictionaryComandline() {
+
+    }
+
+    public static void showAllWords(Dictionary dic) {
+        System.out.printf("%-10s%-20s%-20s\n","No", "| English", "| Vietnamese");
+        for (int i = 0; i < dic.getCntWord(); i++) {
+            System.out.printf("%-10d%-20s%-20s\n",i+1,"| "+dic.getWord(i).getWord_target(),"| "+dic.getWord(i).getWord_explain());
+        }
+    }
+
+    public void dictionaryBasic() {
+        DicM.insertFromCommandline();
+        showAllWords(DicM.getDic());
+    }
+
+    public void dictionaryAdvanced() {
+        DicM.insertFromFile("data\\dictionaries.txt");
+        showAllWords(DicM.getDic());
+        DicM.dictionaryLookup();
+    }
+}
